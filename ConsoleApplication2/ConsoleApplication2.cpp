c23da8eadb108344194d9b134302d97d1e790c86
minislively <blessoms2017@gmail.com>
2021-08-13 14:21:03 +0900

    정올 546번 C++

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a;
+    double sum = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        sum += a; 
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << "avg : " << sum / n << endl;
+    if (sum / n >= 80)
+        cout << "pass";
+    else
+        cout << "fail";
+}
