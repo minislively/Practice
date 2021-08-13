159f44397f059cd055d0810ca4756354da09a128
minislively <blessoms2017@gmail.com>
2021-08-13 14:11:14 +0900

    정올 544번 C++

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0, sum = 0;
+
+    cin >> a;
+
+    for (int i = a; i <= 100; i++)
+    {
+        sum += i;
+    }   
+    cout << sum;
+}
