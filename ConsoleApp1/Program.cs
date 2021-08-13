70c2a484d35fe9492f599e5a5ebac531cd710b39
minislively <blessoms2017@gmail.com>
2021-08-13 11:11:39 +0900

    인터페이스 예제1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IMyInterfaceA
+    {
+        void print();
+    }
+    interface IMyInterfaceB
+    {
+        void print();
+    }
+    class MyClass :IMyInterfaceA, IMyInterfaceB
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+
+            IMyInterfaceA imca = mc;
+            imca.print();
+
+            IMyInterfaceB imcb = mc;
+            imcb.print();
+        }
+        void IMyInterfaceA.print()
+        {
+            Console.WriteLine("IMyInterfaceA.print() 호출.");
+        }
+        void IMyInterfaceB.print()
+        {
+            Console.WriteLine("IMyInterfaceB.print() 호출.");
+        }
+    }
+}
