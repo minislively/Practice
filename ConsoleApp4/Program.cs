a4e2779f3002f65f8bad3cc9660bc34491c8ceaf
minislively <blessoms2017@gmail.com>
2021-08-14 20:13:36 +0900

    정올 547번

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Multi_Interface_Inheritance
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("I Run!");
+
+        }
+        public void Fly()
+        {
+            Console.WriteLine("I Fly!");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Fly();
+            car.Run();
+
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = car as IFlyable;
+            flyable.Fly();
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 2; j < 7; j++)
+		{
+			cout << j + i << " ";
+		}
+		cout << endl;
+	}
+}
