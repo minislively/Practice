ad6cbac2af99cb964b3e3844527e2528c034d839
minislively <blessoms2017@gmail.com>
2021-08-14 20:43:29 +0900

    백준 5596번

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum1 = 0, sum2 = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> a; 
+		sum1 += a; 
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> b;
+		sum2 += b;
+	}
+
+	if (sum1 >= sum2)
+		cout << sum1;
+	else
+		cout << sum2;
+}
