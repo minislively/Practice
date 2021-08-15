ce6237ddf3bf2e3a01b4284df2bebab12208d505
minislively <blessoms2017@gmail.com>
2021-08-15 17:05:32 +0900

    백준 10707번

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, x = 0, y = 0, z = 0;
+
+    cin >> a >> b >> c >> d >> e;
+
+    x = a * e; // x 사에서 한 달 간 사용한 수도 요금 금액 
+
+    if (c >= e) // y사에서 한 달 간 사용한 수도 요금 금액 
+        y = b;
+    if (e > c)
+    {
+        z = e - c;
+        y = b + z * d;
+    }
+
+    if (x > y)
+        cout << y;
+    else
+        cout << x;
+}
