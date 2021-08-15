d2ee717a23df4a51c56f123d3fc12e921c2e414c
minislively <blessoms2017@gmail.com>
2021-08-15 15:13:48 +0900

    정올 131번

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    long double a, b, sum;
+
+    printf("Garo : ");
+    scanf("%Lf", &a);
+    printf("Sero : ");
+    scanf("%Lf", &b);
+
+    sum = a * b;
+    printf("%Lf", sum);
+}
