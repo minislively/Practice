a51041c28f8cd378f47d69db4db7c5c4e7b1eb6e
minislively <blessoms2017@gmail.com>
2021-08-15 15:25:32 +0900

    정올 132번

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, sum = 0;
+
+    cin >> a; 
+
+    for (int i = 0; i <= a; i++)
+    {
+        if ( i%5==0)
+            sum += i;
+    }
+    cout << sum;
+}
