1bdaf2cd1eaf03a84a5e1989184fcdf20d39c0fa
minislively <blessoms2017@gmail.com>
2021-08-15 18:05:38 +0900

    백준 5532번 : 방학 숙제

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int L, A, C, B, D, m=0, l=0;
+
+    cin >> L >> A >> B >> C >> D;
+
+    if (A % C == 0)
+        m = A / C;
+    else
+        m = A / C + 1;
+
+    if (B % D == 0)
+        l = B / D;
+    else
+        l = B / D + 1;
+    
+    if (m >= l)
+        cout << L - m;
+    else
+        cout << L - l;
+}
