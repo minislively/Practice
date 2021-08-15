8ce692abacbbacd77e3a86a8c0f6497a3f53ceb0
minislively <blessoms2017@gmail.com>
2021-08-15 17:31:09 +0900

    부모님 클래스 만들기 예제

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace My
+{
+    class Parent
+    {
+        public int num;
+
+        public Parent()
+        {
+            Console.WriteLine("부모 클래스의 생성자가 호출 되었습니다.");
+
+        }
+    }
+    class Child : Parent
+    {
+        public Child(int num)
+        {
+            base.num = num;
+
+            Console.WriteLine("자식 클래스의 생성자가 호출되었습니다.");
+        }
+        public void DisplayValue()
+        {
+            Console.WriteLine("num의 값은 {0} 입니다", num);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Child cd = new Child(20);
+
+            cd.DisplayValue();
+        }
+    }
+}
