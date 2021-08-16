68857b9437aca4df0ce1733d567f6358444a71ec
minislively <blessoms2017@gmail.com>
2021-08-16 12:10:37 +0900

    정올 135번

---
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, sum = 0;
+
+	double avg = 0, n=0;
+
+	cin >> a >> b;
+	if (a < b)
+	{
+		for (int i = a; i <= b; i++)
+		{
+			if (i % 3 == 0 || i % 5 == 0)
+			{
+				sum += i;
+				n++;
+			}
+		}
+	}
+	else if ( b <= a)
+	{
+		for (int i = b; i <= a; i++)
+		{
+			if (i % 3 == 0 || i % 5 == 0)
+			{
+				sum += i;
+				n++;
+			}
+		}
+	}
+	avg = sum / n;
+	cout << "sum : " << sum << endl;
+	cout << fixed;
+	cout.precision(1);
+	cout << "avg : " << avg << endl;
+}
