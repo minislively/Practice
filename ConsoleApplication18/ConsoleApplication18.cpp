8055c4ce5532be3ee2bf51606d3176d786103ff7
minislively <blessoms2017@gmail.com>
2021-08-16 20:10:05 +0900

    정올 139번

---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    if (a > b)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = a; j >= b; j--)
+            {
+                cout << j << " * " << i << " = " << setw(2) << j*i << "   ";
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = a; j <= b; j++)
+            {
+                cout << j << " * " << i << " = " << setw(2) << j * i << "   ";
+            }
+            cout << endl;
+        }
+    }
+}
