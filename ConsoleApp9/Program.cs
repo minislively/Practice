fc5cd4a66456e6a17900e59ba5118cc4d8598e19
minislively <blessoms2017@gmail.com>
2021-08-16 11:45:27 +0900

    Example 클래스 예제

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Example
+{
+    class Example
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Call Print function");
+        }
+    }
+    class CopyExample1 : Example
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Call new Print function");
+        }
+    }
+    class CopyExample2 : Example
+    {
+        public new void Print()
+        {
+            Console.WriteLine("Call new Print function");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Example clz1 = new CopyExample1();
+            clz1.Print();
+
+            Example clz2 = new CopyExample2();
+            clz2.Print();
+
+            Console.WriteLine("Press any key...");
+            Console.ReadLine();
+        }
+    }
+}
