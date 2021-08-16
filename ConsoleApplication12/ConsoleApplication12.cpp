51d631eaba49feda8c929d7a9cd87fb598b6381e
minislively <blessoms2017@gmail.com>
2021-08-16 11:38:43 +0900

    깊은 복사 예제

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long N, M;
+
+    cin >> N >> M;
+
+    if (N < 0 && M >0)
+        cout << -N + M;
+    else if (N < 0 && M < 0)
+        cout << -N - M;
+    else if (N > 0 && M > 0)
+        cout << N - M;
+    else if (N > 0 && M < 0)
+        cout << N + M;
+    else if (N == M)
+        cout << 0;
+        
+}
