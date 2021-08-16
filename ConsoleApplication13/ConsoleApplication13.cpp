733f9ddb5564e89d4de1e1a19b6172d7cd044b21
minislively <blessoms2017@gmail.com>
2021-08-16 11:57:21 +0900

    정올 134번

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, even=0, odd=0; 
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> a; 
+		if (a % 2 == 0)
+			even++;
+		else
+			odd++;
+	}
+	cout << "even : " << even << endl;
+	cout << "odd : " << odd << endl;
+}
