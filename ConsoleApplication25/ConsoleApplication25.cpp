8ef90a4dbbaa4999344d019e63a27126be2c8c73
minislively <blessoms2017@gmail.com>
2021-08-17 23:02:46 +0900

    백준 15963번

---
+++ ConsoleApplication25/ConsoleApplication25.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    int a = 1, b = 0;
+
+    cin >> n >> m;
+
+    if (n==m)
+        cout << a;
+    else
+        cout << b;
+}
