c2dd7c8acedbc54c3277d333e36f8dd59adc253f
minislively <blessoms2017@gmail.com>
2021-08-17 12:29:44 +0900

    부모 자식 클래스 만들기

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharps
+{
+    class Parent
+    {
+        public virtual void method()
+        {
+            Console.WriteLine("부모 클래스의 메서드 호출!");
+        }
+    }
+    class Child : Parent
+    {
+        public override void method()
+        {
+            Console.WriteLine("자식 클래스의 메서드 호출!");
+        }
+    }
+    class Son : Parent
+    {
+        public override void method()
+        {
+            Console.WriteLine("자식 클래스의 메서드 호출!");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Parent parent = new Parent();
+            parent.method();
+
+            Child child = new Child();
+            child.method();
+
+            Son son = new Son();
+            son.method();
+        }
+    }
+
+}
