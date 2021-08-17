6a550a106ce82df8b1a33649fb0ac33983e0f7ac
minislively <blessoms2017@gmail.com>
2021-08-17 12:44:04 +0900

    정올 549번

---
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0, sum=0, N=0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += i;
+            N++;
+        }
+        if (sum >= n)
+        {
+
+            break;
+        }
+       
+    }
+    cout << N  << " " << sum;
+}
