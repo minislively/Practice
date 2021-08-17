eca68fde645d3845db37752cc954c4a1a3d5995c
minislively <blessoms2017@gmail.com>
2021-08-17 18:09:36 +0900

    정올 551번

---
+++ ConsoleApplication22/ConsoleApplication22.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+
+    cin >> a;
+     
+    for (int i = 1; i <= a; i++)
+    {
+        for (int k = 1; k <= i-1; k++)
+        {
+            cout << " ";
+        }
+        for (int j = a; j >= i; j--)
+        {
+            cout << "*";
+        }
+        cout << endl; 
+       
+    }
+}
