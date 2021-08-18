ada757b25321b5c7e7ff5ade01a8615617b69425
minislively <blessoms2017@gmail.com>
2021-08-18 13:34:37 +0900

    정올 140번

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, sum = 0, n=0;
+
+	for (int i = 0; i < 20; i++)
+	{
+		cin >> a;
+		
+		if (a == 0)
+			break;
+		sum = sum + a; 
+		n++;
+	}
+	cout << sum << " " << sum / n;
+}
