ad5470e79076e57dc1a6681bbd4168b4e9ede951
minislively <blessoms2017@gmail.com>
2021-08-18 10:33:11 +0900

    인터페이스 예제

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace temperature
+{
+    public interface ILogger
+    {
+        void WriteLog(string message);
+    }
+    class ConSoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+        
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0} {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void start()
+        {
+            while(true)
+            {
+                Write("온도를 입력해주세요 : ");
+                string temperature = ReadLine();
+
+                if (temperature == "")
+                    break;
+                logger.WriteLog("현재 온도 : " + temperature);
+
+            }
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor monitor = new ClimateMonitor(new FileLogger("MyLog.txt"));
+            monitor.start();
+        }
+    }
+}
