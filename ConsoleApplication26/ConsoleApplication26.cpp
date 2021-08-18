6ba4a2d8dc9ea64aa4a37c0b5f3c6690c61181f1
minislively <blessoms2017@gmail.com>
2021-08-18 10:24:43 +0900

    깊은 복사

---
+++ ConsoleApplication26/ConsoleApplication26.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    
+    if (a == 2)
+    {
+        if (b == 18)
+            cout << "Special";
+        else if (b<=17)
+            cout << "After";
+        else if (b >= 19)
+            cout << "Before";
+    }
+    else if (a == 1)
+    {
+        cout << "Before";
+    }
+    else if (a > 2)
+    {
+        cout << "After";
+    }
+   
+
+}
