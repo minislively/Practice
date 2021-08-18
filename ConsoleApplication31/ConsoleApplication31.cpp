047338ee557d2f0ec3e7274b10e10f40c91a8d18
minislively <blessoms2017@gmail.com>
2021-08-18 14:40:53 +0900

    정올 141번

---
+++ ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+	cin >> n; 
+
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % n == 0 )
+			cout << i << " ";
+		if (i % n == 0 && i % 10 == 0)
+			break;
+	}
+}
