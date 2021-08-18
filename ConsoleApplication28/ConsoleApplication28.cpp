39e7126da52389839dfd9d5c94b9c02829423089
minislively <blessoms2017@gmail.com>
2021-08-18 13:14:13 +0900

    백준 10953번

---
+++ ConsoleApplication28/ConsoleApplication28.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        char num[4];
+
+        cin >> num;
+
+        cout << (num[0] - '0') + (num[2] - '0') << endl;
+        // char을 int형으로 변환해주기 위해 - '0' 을 해줘야함. 
+    }
+}
