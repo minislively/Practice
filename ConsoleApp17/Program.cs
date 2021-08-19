a262d8bf1bd82493781554b15071c04001d9e4e1
minislively <blessoms2017@gmail.com>
2021-08-19 21:43:41 +0900

    인터페이스 예제

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace InterfaceEx
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Lion lion = new Lion();
+            Camel camel = new Camel();
+
+            person.eat();
+            lion.eat();
+            camel.eat();
+
+            Console.WriteLine();
+
+            EatFunc(person);
+            EatFunc(lion);
+            EatFunc(camel);
+        }
+        private static void EatFunc(object obj)
+        {
+            IAnimal target = obj as IAnimal;
+            if(target != null)
+            {
+                target.eat();
+            }
+        }
+    }
+    class Person : IAnimal
+    {
+        public void eat()
+        {
+            Console.WriteLine("밥을 먹습니다.");
+        }
+    }
+    class Lion : IAnimal
+    {
+        public void eat()
+        {
+            Console.WriteLine("고기를 먹습니다.");
+        }
+    }
+    class Camel
+    {
+        public void eat()
+        {
+            Console.WriteLine("풀을 먹습니다.");
+        }
+    }
+    interface IAnimal
+    {
+        void eat();
+    }
+}
---
+++ ConsoleApplication33/ConsoleApplication33.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[6];
+    int min = 0, sum=0, temp=0;
+
+    for (int i = 0; i < 6; i++)
+    {
+        
+        cin >> array[i];
+
+        if (array[i] < array[i + 1] && i<5)
+        {
+            array[i] = temp;
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+
+    }    
+    cout << array[0] + array[1] + array[2];
+
+}
