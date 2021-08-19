dc4100b9b4ce5a5cdf34e61288cdf17f6d72c67c
minislively <blessoms2017@gmail.com>
2021-08-19 22:38:44 +0900

    백준 16486번

---
+++ ConsoleApplication35/ConsoleApplication35.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long double d1, d2, y=0;
+
+    long double x = 3.141592; // 원주율 
+
+    cin >> d1 >> d2;
+
+    y = 2 * (x * d2 + d1);
+
+    cout << fixed;
+    cout.precision(6);
+    cout << y;
+}
