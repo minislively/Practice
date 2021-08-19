2dc1a18e5193853c60eb258099fc4fb221ceb2d0
minislively <blessoms2017@gmail.com>
2021-08-20 00:20:45 +0900

    백준 11719번

---
+++ ConsoleApplication39/ConsoleApplication39.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str[100];
+
+	for (int i = 0; i < 100; i++)
+	{
+		getline(cin, str[i]); // 공백 포함해서 출력 
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		cout << str[i] << endl; // 공백만 출력 가능 
+	}
+}
