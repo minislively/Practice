cde0b26e093ff9a98ff923acdb2b70b464f93506
minislively <blessoms2017@gmail.com>
2021-08-19 23:54:23 +0900

    정올 145번

---
+++ ConsoleApplication37/ConsoleApplication37.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+	cin >> a;
+
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = 2*i; j < 2*a; j++)
+		{
+			cout << " ";
+		}
+		for (int k = 1; k <= i; k++)
+		{
+			cout << k << " ";
+		}
+		cout << endl;
+	}
+}
