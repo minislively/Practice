62072254e821c1816a153efa99a516306d61923a
minislively <blessoms2017@gmail.com>
2021-08-20 18:17:08 +0900

    정올 147번

---
+++ ConsoleApplication43/ConsoleApplication43.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, n=0;
+
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int k = 1; k < 2*i-1; k++)
+        {
+            cout << " ";
+        }
+
+        for (int j = i; j <= a; j++)
+        {
+            cout << ++n << " ";
+        }
+        
+        cout << endl;
+    }
+}
