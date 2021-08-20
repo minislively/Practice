b361b02bcf8212d78ae9beb724cba35956d43a1e
minislively <blessoms2017@gmail.com>
2021-08-20 18:07:50 +0900

    백준 14470번

---
+++ ConsoleApplication42/ConsoleApplication42.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, n=0, x=0, y=0;
+
+    cin >> a >> b >> c >> d >> e;
+
+    if (a < 0)
+    {
+      cout << -a * c + d + e * b;
+    }
+    else if (a > 0)
+    {
+        cout << (b - a) * e;
+    }
+
+}
