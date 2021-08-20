086436d913713aa0ffc6c2d6d14a07640e96dcd5
minislively <blessoms2017@gmail.com>
2021-08-20 18:43:19 +0900

    정올 555번

---
+++ ConsoleApplication46/ConsoleApplication46.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i];
+    }
+    
+}
