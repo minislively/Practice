d3ae8e1312c9ae6e263f53ca4f81e43eba50a121
minislively <blessoms2017@gmail.com>
2021-08-20 18:27:15 +0900

    정올 148번

---
+++ ConsoleApplication44/ConsoleApplication44.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a; 
+
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "# ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < a; i++)
+    {
+        for (int j = 0; j < 2*i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = i; k < a; k++)
+        {
+            cout << "# ";
+        }
+        cout << endl;
+    }
+}
