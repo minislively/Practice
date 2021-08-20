cf305d7ed7b56c4c4b5c0c2c06b6c2b1b6e8681b
minislively <blessoms2017@gmail.com>
2021-08-20 18:31:14 +0900

    정올 149번

---
+++ ConsoleApplication45/ConsoleApplication45.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a=0;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (a == 5)
+				a = 0;
+			
+			cout << 2 * ++a - 1 << " ";
+		}
+		cout << endl;
+	}
+}
