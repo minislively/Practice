bc77a651927187c81f3ede06818111911ef0378a
minislively <blessoms2017@gmail.com>
2021-08-20 17:11:53 +0900

    백준 13866번

---
+++ ConsoleApplication41/ConsoleApplication41.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int array[4];
+    int temp=0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> array[i];
+    }
+  
+    for (int i = 0; i < 4; i++)
+    {
+       
+        for (int j = i+1; j < 4; j++)
+        {
+            if (array[i] < array[j])
+            {
+                temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    if (array[0] + array[3] < array[1] + array[2])
+        cout << -(array[0] + array[3] - (array[1] + array[2]));
+    else 
+        cout << (array[0] + array[3] - (array[1] + array[2]));
+
+}
