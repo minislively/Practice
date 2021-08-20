f7bf6cc887f5d63ff8647898a9aa836c6e12edca
minislively <blessoms2017@gmail.com>
2021-08-20 21:38:17 +0900

    백준 11721번

---
+++ ConsoleApplication48/ConsoleApplication48.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str = ""; // string 초기화 
+
+    cin >> str;
+
+    for (int i = 0; i <= str.size(); i++) // str.size() string의 사이즈를 반환
+    {                                    // ex) BlockDMask -> 10을 반환 
+        cout << str[i];
+
+        if (i % 10 == 9) 
+            cout << endl;
+        // 10개 출력하면 한 칸 내리기 
+        // 문자열은 0부터 시작하므로, 0~9까지 10개가 출력됨.
+    }
+}
