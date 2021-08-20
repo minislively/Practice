7392d6d442bf476f5167b0a14d98f83c504f7208
minislively <blessoms2017@gmail.com>
2021-08-20 16:02:17 +0900

    백준 14924번

---
+++ ConsoleApplication40/ConsoleApplication40.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long s, t, d, t1=0; // s : 기차 속도 t :파리속도 d : 거리 
+
+    cin >> s >> t >> d;
+
+    t1 = d / (s * 2);
+
+    cout << t1 * t;
+}
