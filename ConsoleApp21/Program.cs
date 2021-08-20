7fd70e5aa82c78d3f67548429a115ffeaace9eea
minislively <blessoms2017@gmail.com>
2021-08-20 12:42:43 +0900

    부모 자식 클래스 예제

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace My
+{
+    class Parent
+    {
+        public int num;
+
+        public Parent()
+        {
+            Console.WriteLine("부모님 호출");
+        }
+    }
+    class Child : Parent
+    {
+        public Child(int num)
+        {
+            this.num = num;
+            Console.WriteLine("자식 호출");
+        }
+        public void DisplayValue()
+        {
+            Console.WriteLine("num의 값은 {0} 입니다.", num);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child(20);
+
+            child.DisplayValue();
+        }
+    }
+}
