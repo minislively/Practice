58e087a562d037f64d235f8e7dcfdfd4f2b70f79
minislively <blessoms2017@gmail.com>
2021-08-21 16:27:28 +0900

    정올 559번

---
+++ ConsoleApplication51/ConsoleApplication51.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b; 
+    double arr[6] = { 85.6, 79.5, 83.1, 80.0, 78.2,75.0 };
+    cin >> a >> b;
+
+    cout << (arr[a - 1] + arr[b - 1]);
+
+
+}
