ceb2710b6dba4f9192250fc734e18e3f7e8d25ff
minislively <blessoms2017@gmail.com>
2021-08-21 16:59:47 +0900

    정올 560번

---
+++ ConsoleApplication52/ConsoleApplication52.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = { 0, };
+	int min = 1000;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+
+		if (arr[i] < min)
+		{
+			min = arr[i];
+
+		}
+	}
+	cout << min;
+}
