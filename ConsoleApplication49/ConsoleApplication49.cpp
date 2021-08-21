493e1b6c89c4b3624fa95db72abff4390c0de5d6
minislively <blessoms2017@gmail.com>
2021-08-21 14:16:11 +0900

    정올 557번

---
+++ ConsoleApplication49/ConsoleApplication49.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch[10]; 
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> ch[i];
+	}
+	cout << ch[0] << " " << ch[3] << " " << ch[6];
+}
