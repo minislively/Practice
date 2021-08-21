ad375ed5390e48c570c5651196ffae312619544f
minislively <blessoms2017@gmail.com>
2021-08-21 17:33:25 +0900

    백준 17388번

---
+++ ConsoleApplication53/ConsoleApplication53.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s, k, h;
+
+    cin >> s >> k >> h;
+
+    if (s + k + h >= 100)
+        cout << "OK";
+
+    else if (s + k + h < 100)
+    {
+        if (s >= h && s >= k && k >= h)
+            cout << "Hanyang";
+        else if (s >= h && s <= k && k >= h)
+            cout << "Hanyang";
+        else if (s >= h && s >= k && k <= h)
+            cout << "Korea";
+        else if (s <= h && s >= k && k <= h)
+            cout << "Korea";
+        else if (s <= h && s <= k && h <= k)
+            cout << "Soongsil";
+        else if (s <= h && s<= k && h>= k)
+            cout << "Soongsil";
+    }
+}
