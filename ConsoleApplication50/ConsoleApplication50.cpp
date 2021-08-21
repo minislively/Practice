2521b124f2c1b41d1fbd4d9a5be66ea4daa4543e
minislively <blessoms2017@gmail.com>
2021-08-21 15:08:11 +0900

    정올 558번

---
+++ ConsoleApplication50/ConsoleApplication50.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+	int arr[100] = { 0, }; // 배열 할당 
+
+	for (int i = 0; i < 100; i++)
+	{
+
+		cin >> arr[i]; // 배열마다 값 집어넣어줌. 
+		
+		if (arr[i] == 0) // 배열이 0이면 for문 탈출 
+			break;
+	}
+	for (int i = 99 ; i >= 0; i--)
+	{
+		if (arr[i] == 0)
+			continue;
+
+		cout << arr[i] << " ";
+	}
+}
