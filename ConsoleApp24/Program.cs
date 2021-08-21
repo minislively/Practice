a71babcb2c1a75033e471e033544cf21a3bb6063
minislively <blessoms2017@gmail.com>
2021-08-21 14:01:06 +0900

    부모 클래스 예제

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace My
+{
+    class Parent
+    {
+        public virtual void A()
+        {
+            Console.WriteLine("부모 클래스 A() 메서드 호출");
+        }
+    }
+    class Child : Parent
+    {
+        public override void A()
+        {
+            Console.WriteLine("자식 클래스(Child)의 A() 메서드 호출");
+        }
+    }
+    class Son : Parent
+    {
+        public override void A()
+        {
+            Console.WriteLine("자식 클래스(Son)의 A() 메서드 호출");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Parent parent = new Parent();
+            parent.A();
+
+            Child child = new Child();
+            child.A();
+
+            Son son = new Son();
+            son.A();
+        }
+    }
+    
+
+}
