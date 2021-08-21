2f50962a90f4db1c7b8626ae77892b8713f83e8a
minislively <blessoms2017@gmail.com>
2021-08-21 18:53:24 +0900

    백준 11549번

---
+++ ConsoleApplication54/ConsoleApplication54.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, num=0;
+    int arr[5];
+
+    cin >> t; // t : tea type 
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] == t)
+            num++;
+    }
+    cout << num;
+}
