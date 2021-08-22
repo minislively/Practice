152335e3447d73620cc224f463930014f1967c48
minislively <blessoms2017@gmail.com>
2021-08-22 15:17:46 +0900

    클래스 예제

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Min
+{
+    public class MyClass
+    {
+        private string name = "Mini";
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            Console.WriteLine("mc.Name : {0}", mc.Name);
+
+            mc.Name = "Bibi";
+            Console.WriteLine("mc.Name : {0}", mc.Name);
+        }
+    }
+}
