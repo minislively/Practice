784997acfebc076f62380f7936edf151e276bb0a
minislively <blessoms2017@gmail.com>
2021-08-22 17:43:12 +0900

    정올 563번

---
+++ ConsoleApplication60/ConsoleApplication60.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    int temp = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i+1; j < 10; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
