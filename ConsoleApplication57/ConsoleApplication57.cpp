1b7957891dede971e18d80770d43bbf52b7a3fa5
minislively <blessoms2017@gmail.com>
2021-08-22 16:03:21 +0900

    백준 17009번

---
+++ ConsoleApplication57/ConsoleApplication57.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long a, b, c, d, e, f; 
+    long long x = 0, y = 0;
+
+    cin >> a >> b >> c >> d >> e >> f;
+    
+    x = 3 * a + 2 * b + 1 * c;
+    y = 3 * d + 2 * e + 1 * f;
+
+    if (x > y)
+        cout << "A";
+    else if (x < y)
+        cout << "B";
+    else
+        cout << "T";
+
+
+}
