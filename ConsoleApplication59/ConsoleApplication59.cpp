af0f9534dbdbf97bf6e2dbaea18946fbf620dd61
minislively <blessoms2017@gmail.com>
2021-08-22 17:30:32 +0900

    정올 562번

---
+++ ConsoleApplication59/ConsoleApplication59.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {0};
+	int s1 = 0, s2 = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+	}
+
+	s2 = arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+	s1 = arr[0] + arr[2] + arr[4] + arr[6] + arr[8];
+ 
+	cout << "sum : " << s2 << endl;
+	cout << "avg : ";
+	cout << fixed;
+	cout.precision(1);
+	cout << (double)s1 / 5 << endl;
+}
