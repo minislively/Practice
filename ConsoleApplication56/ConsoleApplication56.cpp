357bae6b3b3652465dac728b1b70cb67d71bff7b
minislively <blessoms2017@gmail.com>
2021-08-22 15:49:01 +0900

    백준 16204번

---
+++ ConsoleApplication56/ConsoleApplication56.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, m, k, x=0, y=0;
+
+    cin >> n >> m >> k;
+
+    x = m > k ? k : m;
+    y = (n - m) > (n - k) ? (n - k) : (n - m);
+
+    cout << x + y;
+}
