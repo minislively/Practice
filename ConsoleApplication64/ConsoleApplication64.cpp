b90f6b8bd2e3cac2a07a4c3c2f4d628a59f2ec25
minislively <blessoms2017@gmail.com>
2021-08-23 13:59:29 +0900

    정올 152번

---
+++ ConsoleApplication64/ConsoleApplication64.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[10];
+	int odd = 0, even = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+
+		if (i % 2 == 0) // 0, 2, 4 등 짝수 번호를 가진 배열 -> 실상 홀수 자리
+			even = even + arr[i];
+		else // 1 3 5 등 홀수 번호를 가진 배열 -> 실상 짝수 자리
+			odd = odd + arr[i];
+	}
+	cout << "odd : " << even << endl;
+	cout << "even : " << odd << endl;
+}
