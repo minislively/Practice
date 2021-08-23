1aa9cd5809a25291cef418982d0cfc19d92c9806
minislively <blessoms2017@gmail.com>
2021-08-23 13:53:22 +0900

    정올 151번

---
+++ ConsoleApplication63/ConsoleApplication63.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5]; 
+	int sum = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+		if (i==0 || i==2||i==4)
+		{
+			sum += arr[i];
+		}
+	}
+	cout << sum;
+}
