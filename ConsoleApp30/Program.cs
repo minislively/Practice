9fe32cfd3af666b6f192223200f84f3c8dce0da7
minislively <blessoms2017@gmail.com>
2021-08-23 13:39:59 +0900

    깊은 복사 예제

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace my
+{
+    public class Employee
+    {
+        public int No;
+        public string Name;
+
+        public Employee DeepCopy()
+        {
+            Employee employee = new Employee();
+            employee.No = No;
+            employee.Name = Name;
+
+            return employee;
+        }
+    }
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee source = new Employee();
+            source.No = 1;
+            source.Name = "J";
+
+            Employee target = source.DeepCopy();
+            target.No = 2;
+            target.Name = "K";
+
+            Console.WriteLine(source.No + ", " + source.Name);
+            Console.WriteLine(target.No + ", " + target.Name);
+        }
+    }
+}
