0713b58aec60c62ca3a11840f35ce06791006b99
minislively <blessoms2017@gmail.com>
2021-08-23 13:49:36 +0900

    정올 150번

---
+++ ConsoleApplication62/ConsoleApplication62.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch[10]; 
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> ch[i];
+	}
+	for (int i = 9; i >= 0; i--)
+	{
+		cout << ch[i] << " ";
+	}
+}
