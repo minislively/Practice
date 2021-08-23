7cd836692f1d734313b732e4eb5b144187fc3957
minislively <blessoms2017@gmail.com>
2021-08-23 14:13:10 +0900

    정올 153번

---
+++ ConsoleApplication65/ConsoleApplication65.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[100];
+
+	for (int i = 0; i < 100; i++)
+	{
+		cin >> arr[i];
+
+		if (arr[i] == -1)
+		{
+			if(i>=3)
+				cout << arr[i - 3] << " " << arr[i - 2] << " " << arr[i - 1];
+			else if(i==2)
+				cout << arr[i - 2] << " " << arr[i - 1];
+			else if (i==1)
+				cout << arr[i - 1];
+
+			break;
+		}
+	}
+
+}
