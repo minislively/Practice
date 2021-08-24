b768acda342677a59c825335e2ae3b250696f125
minislively <blessoms2017@gmail.com>
2021-08-24 12:32:31 +0900

    백준 2444번

---
+++ ConsoleApplication76/ConsoleApplication76.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int k = i; k < a; k++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2*i-1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 2; i <= a; i++)
+    {
+        for (int k = 2; k <= i; k++)
+        {
+            cout << " ";
+        }
+        for (int j = 2*i-1; j <= 2*a-1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
