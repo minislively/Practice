bba5b3b446c09d7ec057badbfac802dcbe7b7ab8
minislively <blessoms2017@gmail.com>
2021-08-24 12:24:50 +0900

    백준 2523번

---
+++ ConsoleApplication75/ConsoleApplication75.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i < a; i++)
+    {
+        for (int j = i; j < a; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
