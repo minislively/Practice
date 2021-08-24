af6b482e2404029d6cfd034dde6620554e7e69c1
minislively <blessoms2017@gmail.com>
2021-08-24 12:03:29 +0900

    정올 154번

---
+++ ConsoleApplication71/ConsoleApplication71.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, sum = 0;
+
+	for (int i = 0; i < 6; i++)
+	{
+		cin >> x;
+
+		sum = sum + x;
+	}
+	cout << fixed;
+	cout.precision(1);
+	cout << sum/6;
+}
