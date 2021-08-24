3a91adff71b5d5359185532909e06fbd783f65ff
minislively <blessoms2017@gmail.com>
2021-08-24 12:35:18 +0900

    백준 2443번

---
+++ ConsoleApplication77/ConsoleApplication77.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int k = 1; k < i; k++)
+        {
+            cout << " ";
+        }
+        for (int j = 2*i-1; j <= 2*a-1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
