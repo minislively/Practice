6d13465e4497bfb443fdd28118d1a3b5c135a1fe
minislively <blessoms2017@gmail.com>
2021-08-24 12:19:06 +0900

    정올 157번

---
+++ ConsoleApplication74/ConsoleApplication74.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100];
+	int sum = 0, n = 0;
+
+	for (int i = 0; i < 100; i++)
+	{
+		cin >> arr[i];
+
+		if (arr[i] == 0)
+			break;
+
+		if (arr[i] % 5 == 0)
+		{
+			sum = sum + arr[i];
+			n++;
+		}
+	}
+	cout << "Multiples of 5 : " << n << endl;
+	cout << "sum : " << sum << endl;
+	cout << "avg : ";
+	cout << fixed;
+	cout.precision(1);
+	cout << (double)sum / n << endl;
+}
