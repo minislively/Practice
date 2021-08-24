0d0bc4ba740cba2e8cf5be786e001bce42f9ba4a
minislively <blessoms2017@gmail.com>
2021-08-24 17:09:08 +0900

    백준 10991번

---
+++ ConsoleApplication79/ConsoleApplication79.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+	cin >> a;
+
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = i; j < a; j++)
+		{
+			cout << " ";
+		}
+		for (int k = 1; k <= i; k++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
