fac850584283031041aea58fb2bdc80608801055
minislively <blessoms2017@gmail.com>
2021-08-24 12:38:52 +0900

    백준 2522번

---
+++ ConsoleApplication78/ConsoleApplication78.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a; 
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = i; j < a; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < a; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = i; k < a; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
