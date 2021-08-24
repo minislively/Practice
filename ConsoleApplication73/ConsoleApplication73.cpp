d94ca66379a6b06e1dee9c744f0a1d5c876bf670
minislively <blessoms2017@gmail.com>
2021-08-24 12:15:23 +0900

    정올 156번

---
+++ ConsoleApplication73/ConsoleApplication73.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100];
+	int max=0, min=999;
+
+	for (int i = 0; i < 100; i++)
+	{
+		cin >> arr[i];
+
+		if (arr[i] == 999)
+			break;
+
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
+	}
+	cout << "max : " << max << endl;
+	cout << "min : " << min << endl;
+}
