37ed78cb1c5a924350598517ee64b19adfdea71e
minislively <blessoms2017@gmail.com>
2021-08-24 11:49:45 +0900

    고양이 클래스

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine("{0} : 야옹", Name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+            Console.WriteLine("{0} : {1}", kitty.Name, kitty.Color);
+
+            Cat nero = new Cat();
+            nero.Color = "검정색";
+            nero.Name = "네로";
+            Console.WriteLine("{0} : {1}", nero.Name, nero.Color);
+
+        }
+    }
+}
