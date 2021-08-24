970d2f4da7d5136c2eca01155ac44bb149c94d21
minislively <blessoms2017@gmail.com>
2021-08-24 12:09:41 +0900

    정올 155번

---
+++ ConsoleApplication72/ConsoleApplication72.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ch[6] = { 'J', 'U', 'N', 'G', 'O', 'L'};
+	char ch1;
+
+	cin >> ch1;
+
+	if (ch1 == 'J')
+		cout << "0";
+	else if (ch1 == 'U')
+		cout << "1";
+	else if (ch1 == 'N')
+		cout << "2";
+	else if (ch1 == 'G')
+		cout << "3";
+	else if (ch1 == 'O')
+		cout << "4";
+	else if (ch1 == 'L')
+		cout << "5";
+	else
+		cout << "none";
+}
