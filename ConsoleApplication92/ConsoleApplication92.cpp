41f4d404fe3f11fa573f2653907ea214e9b19252
minislively <blessoms2017@gmail.com>
2021-08-26 00:08:51 +0900

    정올 158번

---
+++ ConsoleApplication92/ConsoleApplication92.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100];
+	int n = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		cin >> arr[i];
+
+		if (arr[i] == 0)
+		{
+			break;
+		}
+		n++;
+	}
+	cout << n << endl;
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (arr[i] == 0)
+			break;
+
+		if (arr[i] % 2 == 0)
+		{
+			arr[i] = arr[i] / 2;
+			cout << arr[i] << " ";
+		}
+		else
+		{
+			arr[i] = arr[i] * 2;
+			cout << arr[i] << " ";
+		}
+	}
+}
