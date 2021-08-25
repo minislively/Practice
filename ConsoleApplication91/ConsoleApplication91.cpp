e7f6bcf5d1940643b4fe16051778ab2c216f0026
minislively <blessoms2017@gmail.com>
2021-08-25 23:49:33 +0900

    백준 10992번

---
+++ ConsoleApplication91/ConsoleApplication91.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a; 
+
+    cin >> a;
+    if (a == 1)
+    {
+        cout << "*";
+    }
+    else
+    {
+        for (int i = 1; i < a; i++)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+    for (int i = 2; i < a; i++)
+    {
+        for (int j = i; j < a; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        for (int k = 2; k < 2*i-1; k++)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+    if (a >= 2)
+    {
+        for (int i = 1; i <= 2 * a - 1; i++)
+        {
+            cout << "*";
+        }
+    }
+}
