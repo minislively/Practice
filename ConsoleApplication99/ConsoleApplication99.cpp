bfeb564daf5071a475315a1e07e95213d6f7d3f8
minislively <blessoms2017@gmail.com>
2021-08-26 06:52:33 +0900

    정올 159번

---
+++ ConsoleApplication99/ConsoleApplication99.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[20];
+	int a, temp=0;
+
+	cin >> a;
+
+	for (int i = 0; i < a; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = i+1; j < a; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < a; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
