8da4b689250478c94712634380d77024de9bc031
minislively <blessoms2017@gmail.com>
2021-08-25 15:05:02 +0900

    백준 4153번

---
+++ ConsoleApplication88/ConsoleApplication88.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    while (1)
+    {
+        cin >> a >> b >> c;
+
+        if (a == 0 && b == 0 && c == 0)
+            break;
+
+        if (a > b && a > c)
+        {
+            if (a * a == b * b + c * c)
+                cout << "right" << endl;
+            else
+                cout << "wrong" << endl;
+        }
+        else if (b > a && b > c)
+        {
+            if (b * b == a * a + c * c)
+                cout << "right" << endl;
+            else
+                cout << "wrong" << endl;
+        }
+        else if (c > a && c > b)
+        {
+            if(c*c==a*a+b*b)
+                cout << "right" << endl;
+            else
+                cout << "wrong" << endl;
+
+        }
+    }
+}
