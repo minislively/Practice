24f6294e15b6e25e931299145631637482b67ddb
minislively <blessoms2017@gmail.com>
2021-08-25 23:37:54 +0900

    백준 2576번

---
+++ ConsoleApplication90/ConsoleApplication90.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[7];
+	int min=100, sum=0;
+
+	for (int i = 0; i < 7; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < 7; i++)
+	{
+
+		if (arr[i] % 2 != 0) // 입력한 값이 홀수 값일 때
+		{
+			sum = sum + arr[i];
+			if (arr[i] < min)
+				min = arr[i];
+		}
+	}
+	if (sum != 0 && min != 100)
+	{
+		cout << sum << endl;
+		cout << min << endl;
+	}
+	else
+		cout << -1;
+}
