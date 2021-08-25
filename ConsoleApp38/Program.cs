ac25023c13ef350bf4ce91263dfc9200bbc928e2
minislively <blessoms2017@gmail.com>
2021-08-26 06:06:06 +0900

    강아지 클래스 예제

---
+++ ConsoleApp38/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Dog
+    {
+        public string Name;
+        public string color;
+
+        public void Meow()
+        {
+            Console.WriteLine("{0} : 멍멍", Name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dog mini = new Dog();
+            mini.color = "갈색";
+            mini.Name = "미니";
+            Console.WriteLine("{0} : {1}", mini.color, mini.Name);
+
+            Dog bibi = new Dog();
+            bibi.color = "검은색";
+            bibi.Name = "비비";
+            Console.WriteLine("{0} : {1}", bibi.color, bibi.Name);
+        }
+    }
+}
