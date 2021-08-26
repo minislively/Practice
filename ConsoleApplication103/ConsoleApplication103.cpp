ee00f9782ef0310d17b588250d20a5d45856a3c1
minislively <blessoms2017@gmail.com>
2021-08-26 15:59:48 +0900

    백준 5086번

---
+++ ConsoleApplication103/ConsoleApplication103.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    for (int i = 0; i < 10000; i++)
+    {
+        cin >> a >> b;
+        if (a != 0 && b != 0)
+        {
+            if (a % b == 0)
+                cout << "multiple" << endl;
+            else if (b % a == 0)
+                cout << "factor" << endl;
+            else if (a % b != 0 || b % a != 0)
+                cout << "neither" << endl;
+        }
+        else 
+            break;
+    }
+        
+}
