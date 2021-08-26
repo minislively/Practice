c1494c88c974098d8012954aa6c7076b6bfc2784
minislively <blessoms2017@gmail.com>
2021-08-26 22:53:05 +0900

    백준 2747번

---
+++ ConsoleApplication110/ConsoleApplication110.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int arr[10000];
+
+int print(int a)
+{
+    if (a == 0)
+        return 0;
+    if (a == 1)
+        return 1;
+    if (arr[a] != 0) return arr[a];
+    return arr[a] = print(a - 1) + print(a - 2);
+}
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    int result = print(a);
+}
---
+++ ConsoleApplication111/ConsoleApplication111.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int arr[46];
+
+int print(int a)
+{
+    if (a == 0)
+        return 0;
+    if (a == 1)
+        return 1;
+    if (arr[a] != 0) return arr[a];
+    return arr[a] = print(a - 1) + print(a - 2);
+}
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    cout << print(a);
+}
