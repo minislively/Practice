35074c4a3b87248601ad382332c01ed87974b0cd
minislively <blessoms2017@gmail.com>
2021-08-26 16:55:45 +0900

    백준 10886번

---
+++ ConsoleApplication106/ConsoleApplication106.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a, num1=0, num2=0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        if (a == 0)
+            num1++;
+        else if (a == 1)
+            num2++;
+    }
+    if (num1 > num2)
+        cout << "Junhee is not cute!";
+    else
+        cout << "Junhee is cute!";
+}
