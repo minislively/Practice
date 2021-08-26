c0e3045ad8cdb3cf31d97889e070bacbae7101f7
minislively <blessoms2017@gmail.com>
2021-08-26 18:40:43 +0900

    백준 11726번

---
+++ ConsoleApplication107/ConsoleApplication107.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int d[1001];
+
+int dp(int x)
+{
+    if (x == 1) return 1; // 1일 때 만들 수 있는 타일의 개수 1
+    if (x == 2) return 2; // 2일 때 만들 수 있는 타일의 개수 2
+    if (d[x] != 0) return d[x]; // 0이 아닌 숫자가 있으면 그 값으로 리턴
+    return d[x] = (dp(x - 1) + dp(x - 2)) % 10007; // 길이가 x일 때의 경우의 수를 구한 적이 없으면
+                                                   // 구한 값을 더해줌. %10007인 이유는 값이 커질 수 있는 경우여서
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << dp(x);
+}
