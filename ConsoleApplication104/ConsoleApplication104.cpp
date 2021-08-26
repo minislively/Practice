71cadd91248fe1d12912fac4245b4a74b405d818
minislively <blessoms2017@gmail.com>
2021-08-26 16:40:20 +0900

    백준 5565번

---
+++ ConsoleApplication104/ConsoleApplication104.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10];
+	int sum = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cin >> arr[i];
+
+		if (arr[0] != arr[i])
+			sum = sum + arr[i];
+	}
+	cout << arr[0] - sum;
+
+}
