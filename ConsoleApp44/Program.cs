3965e37dc560f8b1bf7447e693b5762a2db9ee91
minislively <blessoms2017@gmail.com>
2021-08-27 15:25:48 +0900

    사각형 클래스 예제

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace My
+{
+    class Square
+    {
+        private double length;
+        public double GetLength()
+        {
+            return length;
+        }
+        public void SetLength(double length_)
+        {
+            length = length_;
+        }
+        public double GetArea()
+        {
+            return length * length;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+
+            square.SetLength(2);
+
+            Console.WriteLine("사각형의 넓이는 {0} 입니다.", square.GetArea());
+        }
+    }
+}
