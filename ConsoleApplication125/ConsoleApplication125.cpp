772b3b5881f07d835b8633a077c005be14d4189e
minislively <blessoms2017@gmail.com>
2021-08-28 20:16:49 +0900

    정올 173번

---
+++ ConsoleApplication125/ConsoleApplication125.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int print(int a);
+
+int main()
+{
+    int a;
+
+    scanf("%d", &a);
+
+    int result = printf("%d ", print(a));
+}
+
+int print(int a)
+{
+    int n = 0;
+    for (int i = 0; i < a; i++)
+    {
+        for (int i = 0; i < a; i++)
+        {
+            n++;
+        }
+        printf("\n");
+    }
+    return n;
+}
