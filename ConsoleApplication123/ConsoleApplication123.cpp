6a4bcde5d2be69d61376991a4265cabe38239ba9
minislively <blessoms2017@gmail.com>
2021-08-28 17:40:57 +0900

    백준 1085번

---
+++ ConsoleApplication123/ConsoleApplication123.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int print(int x, int y, int w, int h);
+
+int main()
+{
+    int x, y, w, h;
+
+    scanf("%d %d %d %d", &x, &y, &w, &h);
+    int result = printf("%d", print(x, y, w, h));
+}
+
+int print(int x, int y, int w, int h)
+{
+    int min = x;
+
+    if (min > w - x)
+        min = w - x;
+    if (min > y)
+        min = y;
+    if (min > h - y)
+        min = h - y;
+    return min;
+}
