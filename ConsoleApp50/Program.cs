76c416a67d9b443dee1e66b7006a49b237ea21b5
minislively <blessoms2017@gmail.com>
2021-08-29 12:03:12 +0900

    인터페이스 예제

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace My
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Lion lion = new Lion();
+            Camel camel = new Camel();
+
+            person.Eat();
+            lion.Eat();
+            camel.Eat();
+
+            Console.WriteLine();
+
+            EatFunc(person);
+            EatFunc(lion);
+            EatFunc(camel);
+
+        }
+        private static void EatFunc(object obj)
+        {
+            IAnimal target = obj as IAnimal;
+            if(target != null)
+            {
+                target.Eat();
+            }
+        }
+    }
+    class Person : IAnimal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("음식을 먹는다.");
+        }
+    }
+    class Lion : IAnimal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("사슴을 먹는다.");
+        }
+    }
+    class Camel
+    {
+        public void Eat()
+        {
+            Console.WriteLine("물을 마신다.");
+        }
+    }
+    interface IAnimal
+    {
+        void Eat();
+    }
+    
+}
