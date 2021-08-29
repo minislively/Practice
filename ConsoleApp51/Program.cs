2f782a51f3e750dc1835fa061cdcbc2cbebefe6f
minislively <blessoms2017@gmail.com>
2021-08-29 12:10:55 +0900

    개 클래스

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace My
+{
+    class Dog
+    {
+        public string name;
+        public string gender;
+        
+        public void Bark()
+        {
+            Console.WriteLine("{0} : 멍멍!", name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.name = "미미";
+            dog.gender = "여자";
+
+            dog.Bark();
+
+            Console.WriteLine("개의 이름 : {0}, 개의 성별 : {1}", dog.name, dog.gender);
+        }
+    }
+}
