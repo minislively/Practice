e31452f36eddff39089734923bb58ee7c0154e05
minislively <blessoms2017@gmail.com>
2021-08-31 00:04:56 +0900

    백준 14645번

---
+++ ConsoleApplication133/ConsoleApplication133.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a, b;
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d %d", &a, &b);
+
+    printf("비와이");
+}
