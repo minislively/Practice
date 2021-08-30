8008e307c48bce76e1ddf6e03fb7a10ab33a46f6
minislively <blessoms2017@gmail.com>
2021-08-30 14:17:12 +0900

    강아지 클래스 예제

---
+++ ConsoleApp56/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace My
+{
+    class Dog
+    {
+        public string name;
+        public string gender;
+        public string ownernames;
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} : 멍멍!", name);
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.name = "칸";
+            dog.gender = "여자";
+            dog.ownernames = "미나";
+
+            Console.WriteLine("강아지 이름 : {0}, 성별 : {1}, 주인 : {2}", dog.name, dog.gender, dog.ownernames);
+
+            dog.Bark();
+        }
+    }
+}
