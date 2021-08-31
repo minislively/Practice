3c3e937a83b83cd455333911fea4fb6fbdd93a4e
minislively <blessoms2017@gmail.com>
2021-08-31 17:26:49 +0900

    백준 15727번

---
+++ ConsoleApplication150/ConsoleApplication150.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    int t = 0, l = 5;
+
+    scanf("%d", &t);
+
+    if (t % l != 0)
+        printf("%d", t / l + 1);
+    else
+        printf("%d", t / l);
+}
