99ab225f186547ecb80b7668dcfa3c2e48aa2a75
minislively <blessoms2017@gmail.com>
2021-08-31 18:04:47 +0900

    백준 14652번

---
+++ ConsoleApplication152/ConsoleApplication152.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, m; // n:세로 길이 m:가로 길이 
+    int a = 0, b = 0, num = 0;
+    int k; // 관중석 번호 
+
+    scanf("%d %d %d", &n, &m, &k);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (num++ == k)
+            {
+                printf("%d %d", i, j);
+                break;
+            }
+        }
+    }
+}
