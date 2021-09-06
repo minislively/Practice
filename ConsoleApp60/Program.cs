10565ef6f0a473e1ea6e0f4c609f850d993194cb
minislively <blessoms2017@gmail.com>
2021-09-06 22:45:10 +0900

    정올 178번

---
+++ ConsoleApp60/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Min
+{
+    class MainApp
+    {
+        static public void init(ref int num1)
+        {
+            num1 = 100;
+        }
+        static void Main(string[] args)
+        {
+            int num=200;
+            init(ref num);
+
+            Console.WriteLine("{0}", num);
+        }
+    }
+}
---
+++ ConsoleApplication153/ConsoleApplication153.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int print(int a);
+
+int main()
+{
+    int a;
+
+    scanf("%d", &a);
+    printf("%d", print(a));
+}
+
+int print(int a)
+{
+    return 2 << a;
+}
