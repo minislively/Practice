265b9d9db694770071017e7b8387424ee2301b8a
minislively <blessoms2017@gmail.com>
2021-09-13 22:26:12 +0900

    코드업 1092번

---
+++ ConsoleApplication155/ConsoleApplication155.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    double s = 1000, x = 0, y = 0, n = 0, min = 100000;
+
+    scanf("%lf %lf", &x, &y);
+
+    if (s / y * x < min)
+    {
+        min = s / y * x;
+    }
+
+    scanf("%lf", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%lf %lf", &x, &y);
+        if (s / y * x < min)
+        {
+            min = s / y * x;
+        }
+    }
+    printf("%.2lf", min);
+}
