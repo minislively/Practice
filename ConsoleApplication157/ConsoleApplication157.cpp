57b72d4ae95ab27863eb73bb5007b55ee4cbcbb2
minislively <blessoms2017@gmail.com>
2021-09-13 22:40:22 +0900

    백준 2702번

---
+++ ConsoleApplication157/ConsoleApplication157.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    int t, a, b, s = 1, max = 0;
+
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &a, &b);
+
+        while (s % a != 0 || s % b != 0)
+        {
+            s++;
+        }
+        for (int j = 1; j <= 1000; j++)
+        {
+            if (a % j == 0 && b % j == 0)
+            {
+                if (max < j)
+                {
+                    max = j;
+                }
+            }
+        }
+        printf("%d %d\n", s, max);
+        s = 1, max = 0;
+    }
+}
