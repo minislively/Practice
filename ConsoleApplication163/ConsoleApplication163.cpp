606433285304d56e44f3c59ef570ff6452d7cffb
minislively <blessoms2017@gmail.com>
2021-10-09 03:11:31 +0900

    백준 10987번

---
+++ ConsoleApplication163/ConsoleApplication163.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string n = "aeiou";
+    string str;
+    int cnt = 0;
+
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < n.length(); j++)
+        {
+            if (str[i] == n[j])
+                cnt++;
+        }
+    }
+    cout << cnt;
+}
