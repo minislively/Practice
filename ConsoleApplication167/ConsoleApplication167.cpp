d60840b937d44fcf253d8de4f00eeeb9b6ce25e9
minislively <blessoms2017@gmail.com>
2021-10-14 00:29:11 +0900

    백준 4892번

---
+++ ConsoleApplication167/ConsoleApplication167.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i=0; 
+    while (1)
+    {
+        i++;
+        cin >> n;
+        
+        if (n == 0)
+            break;
+
+        n = 3 * n;
+        if (n % 2 == 0)
+        {
+            cout << i << ". even ";
+            n = n / 2;
+        }
+        else if (n % 2 != 0)
+        {
+            cout << i << ". odd ";
+            n = (n + 1) / 2;
+        }
+
+        n = 3 * n;
+
+        n = n / 9;
+        cout << n << '\n';
+    }
+}
