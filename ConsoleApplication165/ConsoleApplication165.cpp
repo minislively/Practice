a28730abca659c266832bdfdca9d4896b521b8d1
minislively <blessoms2017@gmail.com>
2021-10-13 22:50:08 +0900

    백준 2748번

---
+++ ConsoleApplication165/ConsoleApplication165.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+long long int d[90];
+
+long long int fibonacci(int n)
+{
+    d[0] = 0;
+    d[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        d[i] = d[i - 1] + d[i - 2];
+    }
+    return d[n];
+}
+
+int main()
+{
+    int n;
+
+    cin >> n;
+    cout << fibonacci(n);
+}
