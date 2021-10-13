007aacd1f91c3cc7a48df4db74321ed5919812f4
minislively <blessoms2017@gmail.com>
2021-10-14 00:48:07 +0900

    백준 15000번

---
+++ ConsoleApplication169/ConsoleApplication169.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    string str;
+    string A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string a = "abcdefghijklmnopqrstuvwxyz";
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        for (int j = 0; j < a.length(); j++)
+        {
+            if (str[i] == a[j])
+                cout << A[j];
+        }
+    }
+    
+}
