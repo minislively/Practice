95400c1154eac3e8bbe6a957ec4ccae9e6b64bd7
minislively <blessoms2017@gmail.com>
2021-10-17 20:58:44 +0900

    백준 10995번

---
+++ ConsoleApplication173/ConsoleApplication173.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    if (t == 1)
+        cout << "*";
+    else
+    {
+        if (t % 2 == 0)
+        {
+            for (int i = 0; i < t/2; i++) {
+                for (int j = 0; j < t; j++) {
+                    cout << "* ";
+                }
+                cout << '\n';
+                for (int k = 0; k < t; k++) {
+                    cout << " *";
+                }
+                cout << '\n';
+            }
+        }
+        else if (t % 2 != 0)
+        {
+            for (int i = 0; i < t/2; i++) {
+                for (int j = 0; j < t; j++) {
+                    cout << "* ";
+                }
+                cout << '\n';
+                for (int k = 0; k < t; k++) {
+                    cout << " *";
+                }
+                cout << '\n';
+            }
+            for (int k = 0; k < t; k++) {
+                cout << "* ";
+            }
+        }
+    }
+}
