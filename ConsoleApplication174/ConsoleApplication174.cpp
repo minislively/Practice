c20dfdbd4a5f345fba09bb8b99215fa16055a1e5
minislively <blessoms2017@gmail.com>
2021-10-17 21:52:48 +0900

    백준 3034번

---
+++ ConsoleApplication174/ConsoleApplication174.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t, w, h, a;
+    cin >> t >> w >> h;
+
+    int z = w * w + h * h;
+    int x = sqrt(z);
+
+    for (int i = 0; i < t; i++) {
+        cin >> a;
+        if (a <= x)
+            cout << "DA" << '\n';
+        else 
+            cout << "NE" << '\n';
+    }
+}
