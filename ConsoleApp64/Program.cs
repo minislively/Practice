f950931bca78e6ab28f78a04d5f05a2be658d523
minislively <blessoms2017@gmail.com>
2021-10-19 22:39:34 +0900

    인터페이스 예제

---
+++ ConsoleApp64/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace My
+{
+    interface IEmployee
+    {
+        string Name { get; set; }
+        int Counter { get; }
+    }
+    public class Staff:IEmployee
+    {
+        private String name;
+        private int counter;
+        public static int NOofEmployee;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Counter
+        {
+            get { return counter; }
+        }
+        public Staff()
+        {
+            counter = ++counter + NOofEmployee;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("직원 수 입력 : ");
+            Staff.NOofEmployee = int.Parse(Console.ReadLine());
+
+            Staff my = new Staff();
+            Console.Write("신입 직원의 이름을 입력하세요 : ");
+            name = Console.ReadLine();
+            my.Name = name;
+
+            Console.WriteLine(" [ 직원 정보 ] ");
+            Console.WriteLine("직원 정보 : {0}, 직원 이름 {1}", my.Counter, my.Name);
+        }
+    }
+}
