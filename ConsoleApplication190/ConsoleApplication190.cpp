af95f73f5374ec616a4253e519911fd06ff52f14
minislively <blessoms2017@gmail.com>
2021-12-24 23:40:07 +0900

    백준 8760번

---
+++ ConsoleApplication190/ConsoleApplication190.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int z, w, k;
+    cin >> z;
+    for (int i = 0; i < z; i++) {
+        cin >> w >> k;
+        cout << w * k / 2 << '\n';
+    }
+}
