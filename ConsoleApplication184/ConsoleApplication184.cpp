741b0fd06b6b31b2d6ef4c95197d4c78a465a5c7
minislively <blessoms2017@gmail.com>
2021-12-24 11:00:26 +0900

    백준 1864번

---
+++ ConsoleApplication184/ConsoleApplication184.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+int main() {
+    int n, sum=0;
+    string str;
+    cin >> n >> str;
+    for (int i = 0; i < n; i++) {
+        if (isdigit(str[i]))
+            sum = sum + isdigit(str[i]);
+    }
+    cout << sum;
+}
---
+++ ConsoleApplication185/ConsoleApplication185.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    long long int cnt = 1;
+    unsigned long long int k = 1;
+    long long int sum = 0;
+    while (1) {
+        cin >> s;
+        if (s == "#")
+            break;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == '-')
+                cnt = 0;
+            else if (s[i] == '\\')
+                cnt = 1;
+            else if (s[i] == '(')
+                cnt = 2;
+            else if (s[i] == '@')
+                cnt = 3;
+            else if (s[i] == '?')
+                cnt = 4;
+            else if (s[i] == '>')
+                cnt = 5;
+            else if (s[i] == '&')
+                cnt = 6;
+            else if (s[i] == '%')
+                cnt = 7;
+            else if (s[i] == '/')
+                cnt = -1;
+            sum = sum + cnt * k;
+            k = k * 8;
+        }
+        cout << sum << '\n';
+        sum = 0, k = 1, cnt = -1;
+    }
+}
