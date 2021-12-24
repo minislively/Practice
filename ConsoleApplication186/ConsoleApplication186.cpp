9143a92272def91c6ab2ed2e5a47b823ba0ce386
minislively <blessoms2017@gmail.com>
2021-12-24 13:32:39 +0900

    백준 4949번

---
+++ ConsoleApplication186/ConsoleApplication186.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    while (1) {
+        string s;
+        stack<char> st;
+        bool check = 0;
+        getline(cin, s);
+        if (s == ".")
+            break;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '[')
+                st.push(s[i]);
+            else if (s[i] == ')') {
+                if (!st.empty() && st.top() == '(')
+                    st.pop();
+                else
+                    check = 1;
+            }
+            else if (s[i] == ']') {
+                if (!st.empty() && st.top() == '[')
+                    st.pop();
+                else
+                    check = 1;
+            }
+        }
+        if (check == 0 && st.empty())
+            cout << "yes" << '\n';
+        else
+            cout << "no" << '\n';
+    }
+}
