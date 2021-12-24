0b660cd1977f8bc910f587e24d8b43ad608b8bae
minislively <blessoms2017@gmail.com>
2021-12-24 23:33:11 +0900

    백준 5988번

---
+++ ConsoleApplication189/ConsoleApplication189.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    int n, a;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        if ((s[s.length() - 1]-'0') % 2 != 0)
+            cout << "odd" << '\n';
+        else
+            cout << "even" << '\n';
+    }
+}
