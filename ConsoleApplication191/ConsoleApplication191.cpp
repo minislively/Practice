f50be00e3043f825e8b352347e60fa98352973c7
minislively <blessoms2017@gmail.com>
2021-12-25 00:28:01 +0900

    백준 17284번

---
+++ ConsoleApplication191/ConsoleApplication191.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n = 5000, a;
+    while (cin >> a) {
+        if (a == 1)
+            n -= 500;
+        else if (a == 2)
+            n -= 800;
+        else if (a == 3)
+            n -= 1000;
+    }
+    cout << n << '\n';
+}
