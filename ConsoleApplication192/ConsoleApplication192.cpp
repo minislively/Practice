7f4942aa57bbc04c58425b20e2daf2703e1d0f2b
minislively <blessoms2017@gmail.com>
2021-12-25 00:48:09 +0900

    백준 11320번

---
+++ ConsoleApplication192/ConsoleApplication192.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    double t, a, b;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> a >> b;
+        cout << fixed;
+        cout.precision(0);
+        cout << (a * a / 2) / (b * b / 2) << '\n';
+    }
+}
