4307ec845e4300fe996b6456e5c7b335ca9ceee5
minislively <blessoms2017@gmail.com>
2021-12-26 00:10:12 +0900

    백준 7120번

---
+++ ConsoleApplication203/ConsoleApplication203.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    cout << s[0];
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i] != s[i - 1])
+            cout << s[i];
+    }
+}
