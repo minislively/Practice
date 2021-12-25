ddb1db31b23248ed82e824a8d50f5a0172550805
minislively <blessoms2017@gmail.com>
2021-12-25 21:35:51 +0900

    백준 11772번

---
+++ ConsoleApplication198/ConsoleApplication198.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int a[5];
+    int x = 0, temp = 0;
+    for (int i = 0; i < 5; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++) {
+        x = a[i];
+        for (int j = 0; i < 5; j++) {
+            a[i] = a[j];
+            a[j] = x;
+        }
+        cout << a[0] << " " << a[1] << " " << a[2] << " " << a[3] << " " << a[4] << '\n';
+    }
+
+}
