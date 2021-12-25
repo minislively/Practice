af4728387fc435f0523c4ad23f37c6523a6b76ab
minislively <blessoms2017@gmail.com>
2021-12-25 21:45:34 +0900

    백준 15096번

---
+++ ConsoleApplication198/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    double n, sum = 0, a, cnt = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a == 3) {
+            sum += 3;
+            cnt++;
+        }
+        if (a == 2) {
+            sum += 2;
+            cnt++;
+        }
+        if (a == 1) {
+            sum += 1;
+            cnt++;
+        }
+        if (a == 0) {
+            sum += 0;
+            cnt++;
+        }
+        if (a == 4) {
+            sum += 4;
+            cnt++;
+        }
+    }
+    cout << sum / cnt << '\n';
+}
