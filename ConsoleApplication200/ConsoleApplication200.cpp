4986ec3dce3e30b3d0cb40b3ff45aab850a850cc
minislively <blessoms2017@gmail.com>
2021-12-25 22:18:37 +0900

    백준 15178번

---
+++ ConsoleApplication200/ConsoleApplication200.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n, a, b, c, sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b >> c;
+        sum = a + b + c;
+        if (sum != 180)
+            cout << a << " " << b << " " << c << " " << "Check" << '\n';
+        if (sum == 180)
+            cout << a << " " << b << " " << c << " " << "Seems OK" << '\n';
+    }
+}
