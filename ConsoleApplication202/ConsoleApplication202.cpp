4a5a26a81cfe1aa810062d1f73c05fdabaedbf6f
minislively <blessoms2017@gmail.com>
2021-12-25 23:39:04 +0900

    백준 17945번

---
+++ ConsoleApplication202/ConsoleApplication202.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x = -a + sqrt(a * a - b);
+    int y = -a - sqrt(a * a - b);
+    if (x != y)
+        cout << y << " " << x << '\n';
+    else
+        cout << x << '\n';
+}
