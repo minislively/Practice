d97ae0f4cb45ca9017dc754b9af7e083f8ebb972
minislively <blessoms2017@gmail.com>
2021-12-25 21:54:53 +0900

    백준 14782번

---
+++ ConsoleApplication199/ConsoleApplication199.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    // 약수를 구해야한다. 
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+    cout << sum << '\n';
+}
