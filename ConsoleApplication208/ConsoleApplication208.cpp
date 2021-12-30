fdf0424fa796a3ef168726a3c3148bdf4ba11966
minislively <blessoms2017@gmail.com>
2021-12-30 22:35:57 +0900

    백준 3053번

---
+++ ConsoleApplication208/ConsoleApplication208.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int r;
+    long double pi = 3.1415926535897932;
+    cin >> r;
+    cout << fixed;
+    cout.precision(6);
+    cout << r * r * pi << '\n';
+    cout << r*r*2 << '\n';
+}
