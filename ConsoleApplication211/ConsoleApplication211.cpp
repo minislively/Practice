3803c8eca10b9b84496b4fdef89537f7c046ac62
minislively <blessoms2017@gmail.com>
2022-01-02 20:40:19 +0900

    백준 2839번

---
+++ ConsoleApplication211/ConsoleApplication211.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    while (n>=0) {
+        if (n % 5 == 0 && n>=0) {
+            sum += n / 5;
+            cout << sum << '\n';
+            return 0;
+        }
+        n -= 3;
+        sum++;
+    }
+    cout << -1 << '\n';
+}
