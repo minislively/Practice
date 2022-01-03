bbdaf0618abf83ebc3ed242a5bbd3fa8382fe483
minislively <blessoms2017@gmail.com>
2022-01-03 12:41:11 +0900

    정올 589번

---
+++ ConsoleApplication215/ConsoleApplication215.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int SumFunction(int n) {
+    if (n >= 0) return n + SumFunction(n - 1);
+    return 0;
+}
+
+int main() {
+    int n = 0;
+    cin >> n;
+    cout << SumFunction(n);
+}
