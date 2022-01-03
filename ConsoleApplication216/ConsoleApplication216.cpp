8e523c63acaedfbee31cf85962f4ff0e6b8fb94f
minislively <blessoms2017@gmail.com>
2022-01-03 12:59:31 +0900

    정올 592번

---
+++ ConsoleApplication216/ConsoleApplication216.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+int SquaredSum(int n) {
+    if (n == 0)return 0;
+    return (((n % 10) * (n % 10)) + SquaredSum(n / 10));
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << SquaredSum(n);
+}
