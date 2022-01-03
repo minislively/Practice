6d4c8fd6fa824943dd79c03df8bf53c27d4b6449
minislively <blessoms2017@gmail.com>
2022-01-03 12:24:11 +0900

    코드업 1085번

---
+++ ConsoleApplication213/ConsoleApplication213.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+using namespace std;
+int main(){
+    double h, b, c, s;
+    cin >> h >> b >> c >> s;
+    cout << fixed;
+    cout.precision(1);
+    cout << (h * b * c * s) / (8*1024*1024) << " MB";
+}
