9d51a9493faa42a85eceb584b6a96fb963e841cc
minislively <blessoms2017@gmail.com>
2022-01-04 23:51:34 +0900

    정올 596번

---
+++ ConsoleApplication218/ConsoleApplication218.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    int n, cnt = 0;
+    cin >> s >> n;
+    for (int i = s.length() - 1; i >= 0; i--) {
+        if (cnt == n) break;
+        else cout << s[i];
+        cnt++;
+    }
+}
