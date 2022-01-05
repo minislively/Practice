5242c4e09fd6f0157f3f4be0551727cefbe1f0be
minislively <blessoms2017@gmail.com>
2022-01-05 10:13:02 +0900

    백준 15921번

---
+++ ConsoleApplication219/ConsoleApplication219.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n, a;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a;
+    if (n == 0) cout << "divide by zero";
+    else cout << "1.00";
+}
