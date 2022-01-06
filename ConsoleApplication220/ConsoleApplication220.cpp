dbcf836c5fa505dbfe2ccc08ae32733cb9d3af6a
minislively <blessoms2017@gmail.com>
2022-01-06 23:38:26 +0900

    백준 5597번

---
+++ ConsoleApplication220/ConsoleApplication220.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int a[31] = { 0, };
+    int m;
+    for (int i = 0; i < 28; i++) {
+        cin >> m;
+        a[m] = a[m] + 1;
+    }
+    for (int i = 1; i <= 30; i++)
+        if (a[i] == 0) cout << i << '\n';
+}
---
+++ Practice1/Practice1.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int n = 0;
+    char s[100] = { 0, };
+
+    scanf_s("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        char s[100] = { 0, };
+        int sum = 0, add = 1;
+        scanf_s("%s", s);
+
+        for (int j = 0; j < strlen(s); j++)
+        {
+            if (s[j] == 'O')
+            {
+                sum += add;
+                add++;
+            }
+            if (s[j] == 'X')
+            {
+                add = 1;
+            }
+        }
+        printf_s("%d\n", sum);
+    }
+}
