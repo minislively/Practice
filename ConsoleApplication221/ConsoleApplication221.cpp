57845f54d06d9c318188681ccead23bae8167a59
minislively <blessoms2017@gmail.com>
2022-01-07 23:55:01 +0900

    백준 4619번

---
+++ ConsoleApplication221/ConsoleApplication221.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    while (1) {
+        int b, n;
+        cin >> b >> n;
+        if (b == 0 && n == 0) break;
+        for (int i = 1; i <= b; i++) {
+            unsigned long long int x = pow(i, n);
+            if (x >= b) {
+                unsigned long long int y = pow(i - 1, n);
+                if (x - b > b - y) cout << i - 1 << '\n';
+                else if (x - b <= b - y) cout << i << '\n';
+                break;
+            }
+        }
+    }
+}
