18cee17f011773e5bb0ed8ce25f3850821e59f8b
minislively <blessoms2017@gmail.com>
2022-01-11 11:48:55 +0900

    백준 1225번

---
+++ ConsoleApplication225/ConsoleApplication225.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    unsigned long long int j = 0, sum = 0;
+    string a, b;
+    cin >> a >> b;
+    int x = b.length() - 1;
+    while (1) {
+        if (j == (x+1)) break;
+        for (int i = 0; i < a.length(); i++) {
+            sum += (a[i] - '0')*(b[j]-'0');
+        }
+        j++;
+    }
+    cout << sum << '\n';
+}
