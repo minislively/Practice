60102806ec19d196da48ccf36b1b4fdeb25ecb28
minislively <blessoms2017@gmail.com>
2022-01-16 01:52:17 +0900

    백준 24087번

---
+++ ConsoleApplication230/ConsoleApplication230.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int s, a, b;
+    cin >> s >> a >> b;
+    if (s > a) {
+        int x = s - a;
+        if (x % b == 0) 
+            cout << 250 + 100 * (x / b);
+        else
+            cout << 250 + 100 * (x / b) + 100;
+    }
+    else
+        cout << 250;
+}
