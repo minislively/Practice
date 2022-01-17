51d4e8231903cbe0d8ab85f9aa3f3a3f24314d1d
minislively <blessoms2017@gmail.com>
2022-01-17 22:32:54 +0900

    백준 2231번

---
+++ ConsoleApplication232/ConsoleApplication232.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    int sum;
+    int part = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        sum = i; // n의 합 
+        part = i; // n의 자리수들을 더할 부분
+        while (1) {
+            if (part == 0) break;
+            sum += part % 10; // 각각 자릿수의 합을 구함. 
+            part /= 10;
+        }
+        if (n == sum) {
+            cout << i << '\n';
+            return 0;
+        }
+    }
+    cout << "0" << '\n';
+}
