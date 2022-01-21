828d8d4625c46ff808634a13e748445fc5f2b680
minislively <blessoms2017@gmail.com>
2022-01-21 15:36:28 +0900

    백준 24075번

---
+++ ConsoleApplication237/ConsoleApplication237.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x = a + b;
+    int y = a - b;
+    if (x > y) cout << x << '\n' << y << '\n';
+    else cout << y << '\n' << x << '\n';
+}
