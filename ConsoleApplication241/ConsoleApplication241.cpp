a90f743503d3be9d366105364e42b59a2048f77e
minislively <blessoms2017@gmail.com>
2022-01-27 01:12:31 +0900

    백준 14935번

---
+++ ConsoleApplication241/ConsoleApplication241.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            sum += i * j;
+        }
+    }
+    cout << sum << '\n';
+}
---
+++ ConsoleApplication242/ConsoleApplication242.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string x;
+    int a = 0;
+    cin >> x;
+    while (1) {
+        int y = x.length() * (x[0] - '0');
+        if (a == y) {
+            cout << "FA" << '\n';
+            break;
+        }
+        a = y;
+    }
+}
