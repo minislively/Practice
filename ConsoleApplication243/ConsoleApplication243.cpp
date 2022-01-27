01cfabfa89751c6654f2e70902113eee140de5b2
minislively <blessoms2017@gmail.com>
2022-01-28 02:13:39 +0900

    백준 24265번

---
+++ ConsoleApplication243/ConsoleApplication243.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    long long sum = 0, n, cnt = 0;
+    cin >> n;
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = i + 1; j <= n; j++)
+            cnt++;
+    }
+    cout << cnt << '\n' << 2 << '\n';
+}
