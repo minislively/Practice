cf93ab2a0c7c9e34ef5105d2a2dd6be6012ee6af
minislively <blessoms2017@gmail.com>
2022-01-30 01:25:18 +0900

    백준 24083번

---
+++ ConsoleApplication245/ConsoleApplication245.cpp
@@ -0,0 +1,9 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x = (a + b) % 12;
+    if (x == 0) cout << 12 << '\n';
+    else cout << x << '\n';
+}
