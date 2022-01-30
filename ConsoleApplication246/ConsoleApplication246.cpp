c2010a2de15c2ae79ef2aa9f01d043660f821ec2
minislively <blessoms2017@gmail.com>
2022-01-30 23:54:09 +0900

    백준 14173번

---
+++ ConsoleApplication246/ConsoleApplication246.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+    int a1[4], a2[4];
+    for (int i = 0; i < 4; i++) {
+        cin >> a1[i] >> a2[i];
+    }
+    sort(a1, a1 + 4);
+    sort(a2, a2 + 4);
+    int x = a1[3] - a1[0];
+    int y = a2[3] - a2[0];
+    if (x > y)cout << x * x << '\n';
+    else if (x < y) cout << y * y << '\n';
+}
