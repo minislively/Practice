d86f6286e9d44b73feaa193ea89c47110e898098
minislively <blessoms2017@gmail.com>
2022-01-31 23:14:17 +0900

    백준 10180번

---
+++ ConsoleApplication247/ConsoleApplication247.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int t, n, d, v, f, c;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int sum = 0;
+        cin >> n >> d;
+        for (int j = 0; j < n; j++) {
+            cin >> v >> f >> c;
+            if (v * f / c >= d)sum++;
+        }
+        cout << sum << '\n';
+    }
+}
