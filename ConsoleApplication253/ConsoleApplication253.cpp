33ac3b64a4dcebfb931ddbd3099c049c2d5b1215
minislively <blessoms2017@gmail.com>
2022-02-07 23:05:03 +0900

    백준 7789번

---
+++ ConsoleApplication253/ConsoleApplication253.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b, check = 0;
+    cin >> a >> b;
+    b = a + (b * 1000000);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 2; j < a; j++) {
+            if (a % j == 0) {
+                cout << "No" << '\n';
+                return 0;
+            }
+        }
+        for (int k = 2; k < b; k++) {
+            if (b % k == 0) {
+                cout << "No" << '\n';
+                return 0;
+            }
+        }
+    }
+    if (check == 0) cout << "Yes" << '\n';
+}
