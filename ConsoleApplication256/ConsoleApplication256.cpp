89bbc69faffacd22db60152e17a431ead8e1dad7
minislively <blessoms2017@gmail.com>
2022-02-12 01:51:26 +0900

    백준 24267번

---
+++ ConsoleApplication256/ConsoleApplication256.cpp
@@ -0,0 +1,8 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    long long n;
+    cin >> n;
+    cout << ((n - 1) * (n - 2) * (2 * n - 3) + 3 * (n - 1) * (n - 2)) / 12 << '\n';
+    cout << 3 << '\n';
+}
