ce6ef74dcea523a01af722af8e44176bee6c83b3
minislively <blessoms2017@gmail.com>
2022-02-14 00:55:00 +0900

    백준 15829번

---
+++ ConsoleApplication257/ConsoleApplication257.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    long long a, b, x = 0, y = 0;
+    cin >> a >> b;
+    x = (a * (a + 1)) / 2;
+    y = (b * (b + 1)) / 2;
+    cout << x << '\n';
+    cout << y << '\n';
+}
---
+++ ConsoleApplication258/ConsoleApplication258.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+unsigned long long mod = 1234567891, sum = 0, cnt = 1;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n; i++) {
+        sum += (s[i] - 'a' + 1) * cnt;
+        sum %= mod;
+        cnt *= 31;
+        cnt %= mod;
+    }
+    cout << sum << '\n';
+}
