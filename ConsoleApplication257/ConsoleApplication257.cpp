b9ff517e6dfbb5e744feab5a2f9cddd53b45082b
minislively <blessoms2017@gmail.com>
2022-02-15 01:16:42 +0900

    백준 3460번

--- ConsoleApplication257/ConsoleApplication257.cpp
+++ ConsoleApplication257/ConsoleApplication257.cpp
@@ -1,10 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
 int main() {
-    long long a, b, x = 0, y = 0;
-    cin >> a >> b;
-    x = (a * (a + 1)) / 2;
-    y = (b * (b + 1)) / 2;
-    cout << x << '\n';
-    cout << y << '\n';
+    int a, b, v;
+    int sum = 0;
+    cin >> a >> b >> v;
+    sum = (v - b - 1) / (a - b);
 }
---
+++ ConsoleApplication259/ConsoleApplication259.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n, a;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        int cnt = 0;
+        while (a>0) {
+            if (a % 2 == 1) // 
+                cout << cnt << " ";
+            a /= 2;
+            cnt++; // 최하비트 
+        }
+    }
+}
