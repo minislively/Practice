3cf26b89f3cb26ae8d463b658c32d99681075315
minislively <blessoms2017@gmail.com>
2022-02-16 01:16:37 +0900

    백준 5586번

---
+++ ConsoleApplication260/ConsoleApplication260.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    int n = 0, m = 0;
+    cin >> s;
+    for (int i = 0; i < s.length() - 2; i++) {
+        if (s[i] == 'J' && s[i+1] == 'O' && s[i+2] == 'I')
+            n++;
+        if (s[i] == 'I' && s[i+1] == 'O' && s[i+2] == 'I')
+            m++;
+    }
+    cout << n << '\n' << m << '\n';
+}
