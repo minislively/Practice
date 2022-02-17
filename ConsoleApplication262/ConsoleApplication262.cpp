c29fe33926f1ca5983b748bdb063d6d2b7ea36ad
minislively <blessoms2017@gmail.com>
2022-02-18 02:21:48 +0900

    백준 15726번

---
+++ ConsoleApplication262/ConsoleApplication262.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    double a, b, c, x, y;
+    cin >> a >> b >> c;
+    x = a * b / c;
+    y = a / b * c;
+    if (x > y) cout << (int) x << '\n';
+    else cout << (int) y << '\n';
+}
