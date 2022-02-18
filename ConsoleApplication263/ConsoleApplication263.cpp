59a85e5ea182e2a7635b300d382e28f6a7ee6f49
minislively <blessoms2017@gmail.com>
2022-02-19 03:46:00 +0900

    백준 14264번

---
+++ ConsoleApplication263/ConsoleApplication263.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    double l;
+    cin >> l;
+    cout << fixed;
+    cout.precision(10);
+    cout << l * l * sqrt(3) / 4;
+}
