8b5a4145a2ee16e6b027f430c9576e5dccc4f024
minislively <blessoms2017@gmail.com>
2022-02-21 00:57:52 +0900

    백준 11024번

---
+++ ConsoleApplication264/ConsoleApplication264.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;     // cin 입력 버퍼에는 정수 t와 개행문자가 들어간다.
+    cin.ignore(); // 입력 버퍼를 비워줌. 
+    while (t--) {
+        string s;
+        int sum = 0;
+        getline(cin, s);
+        string tmp = "";
+        for (int i = 0; i < s.length(); i++) {
+            tmp += s[i];
+            if (s[i] == ' ' || i == s.length() - 1) {
+                sum += stoi(tmp);
+                tmp = "";
+            }
+        }
+        cout << sum << '\n';
+    }
+}
