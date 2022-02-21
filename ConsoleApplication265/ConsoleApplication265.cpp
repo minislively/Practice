94c1f59f0a95b9708c31af96b8d35a84ea313bf7
minislively <blessoms2017@gmail.com>
2022-02-22 02:18:54 +0900

    백준 14581번

---
+++ ConsoleApplication265/ConsoleApplication265.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << s << "::fan:" << '\n';
+    cout << ":fan::fan::fan:" << '\n';
+}
