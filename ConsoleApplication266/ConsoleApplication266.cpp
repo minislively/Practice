2666fb0c54052304ece94e25e30cd7d04ecf74c1
minislively <blessoms2017@gmail.com>
2022-02-23 04:24:48 +0900

    백준 20215번

---
+++ ConsoleApplication266/ConsoleApplication266.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    int w, h;
+    cin >> w >> h;
+    double n = (w + h) - sqrt(w * w + h * h);
+    cout << fixed;
+    cout.precision(6);
+    cout << n << '\n';
+}
