70599637eb93009c08a0dc5b5bd87d5774c16485
minislively <blessoms2017@gmail.com>
2022-02-25 00:09:11 +0900

    백준 15080번

---
+++ ConsoleApplication268/ConsoleApplication268.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    int x, y, z;
+    scanf("%d : %d : %d", &a, &b, &c);
+    scanf("%d : %d : %d", &x, &y, &z);
+    int m = x * 3600 + y * 60 + z;
+    int n = a * 3600 + b * 60 + c;
+    if (n > m) printf("%d", m - n + 3600 * 24);
+    else printf("%d", m - n);
+}
