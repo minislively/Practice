5488f5a17a8fa6f24aadd4e35237fb6fc30f7dc3
minislively <blessoms2017@gmail.com>
2022-02-26 02:02:45 +0900

    백준 21633번

---
+++ ConsoleApplication269/ConsoleApplication269.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    double n;
+    cin >> n;
+    double m = 25 + n * 0.01;
+    if (m >= 100)
+        m = m;
+    else
+        m = 100.00;
+    if (m < 2000)
+        m = m;
+    else
+        m = 2000;
+    cout << fixed;
+    cout.precision(2);
+    cout << m << '\n';
+}
