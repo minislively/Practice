5a03f37dc14b4c60611c84dd72a43b72ad6d4f5d
minislively <blessoms2017@gmail.com>
2022-03-13 04:47:08 +0900

    백준 24294번

---
+++ ConsoleApplication270/ConsoleApplication270.cpp
@@ -0,0 +1,8 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+    int w1, h1, w2, h2;
+    cin >> w1 >> h1 >> w2 >> h2;
+    cout << 2 * max(w1, w2) + 2 * (h1 + h2) + 4 << '\n';
+}
