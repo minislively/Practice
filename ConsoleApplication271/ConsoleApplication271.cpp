dbd7ac87e0714c32494743f0e85cd564a217d113
minislively <blessoms2017@gmail.com>
2022-03-13 22:35:47 +0900

    백준 21335번

---
+++ ConsoleApplication271/ConsoleApplication271.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+const double PI = 3.14159265359;
+int main() {
+    double a;
+    cin >> a;
+    double m = sqrt(a / PI) * 2 * PI;
+    cout << fixed;
+    cout.precision(6);
+    cout << m << '\n';
+}
