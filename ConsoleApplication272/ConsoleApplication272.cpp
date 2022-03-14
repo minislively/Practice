2b3d96d342334a8ef1d49359a138c7f511497f47
minislively <blessoms2017@gmail.com>
2022-03-15 01:26:33 +0900

    백준 18414번

---
+++ ConsoleApplication272/ConsoleApplication272.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int x, l, r, min = 0, s = 0, y = 0;
+    cin >> x >> l >> r;
+    if (x <= l)
+        cout << l << '\n';
+    else if (x >= r)
+        cout << r << '\n';
+    else
+        cout << x << '\n';
+}
